26d63e443ec2b58502dd6254d1d553672385b3b1
Guilherme Morais <gui.lopes@gmail.com>
2019-11-09 17:17:46 +0100

    Add the final class and an extension to convert to it.

---
+++ HackerNews/Extensions/PostDtoExtensions.cs
@@ -0,0 +1,32 @@
+﻿using HackerNews.Models;
+
+namespace HackerNews.Extensions
+{
+    public static class PostDtoExtensions
+    {
+        /// <summary>
+        /// Convert a <see cref="PostDto"/> to a <see cref="Post"/>
+        /// </summary>
+        /// <param name="dto">Dto to be converted</param>
+        /// <returns>
+        /// New Object of type <see cref="Post"/>.
+        /// <remarks>Can return null if Dto is null</remarks>
+        /// </returns>
+        public static Post MapToPost(this PostDto dto)
+        {
+            if (dto == null)
+            {
+                return null;
+            }
+
+            return new Post
+            {
+                Author = dto.By,
+                Comments = dto.Descendants,
+                Points = dto.Score,
+                Title = dto.Title,
+                Uri = dto.Url
+            };
+        }
+    }
+}
---
+++ HackerNews/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HackerNews.Models
+{
+    public class Post
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("uri")]
+        public string Uri { get; set; }
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("points")]
+        public int Points { get; set; }
+
+        [JsonPropertyName("comments")]
+        public int Comments { get; set; }
+
+        [JsonPropertyName("rank")]
+        public int Rank { get; set; }
+    }
+}
